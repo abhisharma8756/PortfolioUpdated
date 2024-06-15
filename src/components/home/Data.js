1c289558748b35f39447f10630fa688d28403984
Abhishek Vishwakarma <103137932+abhisharma8756@users.noreply.github.com>
2024-06-15 13:45:09 +0530

    Data.js

--- src/components/home/Data.js
+++ src/components/home/Data.js
@@ -8,7 +8,7 @@ const Data = () => {
   return (
     <div className='home__data'>
       <h1 className='home__title'>
-        Abhishek
+        Ramu
         <svg
                   width="36"
                   height="36"
